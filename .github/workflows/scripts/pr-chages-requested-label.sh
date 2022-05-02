3eec25beeaf7b5707d4a0eaad833863604913f99
Alessandro Aglietti <alessandro.aglietti@brumbrum.it>
2022-05-02 11:31:23 +0200

    ci(pr-changes-requested-label): split labeler execetion with workflow_run to get GITHUB_TOKEN rights

--- .github/workflows/scripts/pr-chages-requested-label.sh
+++ .github/workflows/scripts/pr-chages-requested-label.sh
@@ -8,40 +8,57 @@ date
 gh auth status
 
 # env passed by GitHub Action environment
+# GITHUB_REF_NAME="master"
+# SOURCE_RUN_WORKFLOW_ID=2259649471
+# GITHUB_API_URL="https://api.github.com"
+# GITHUB_REPOSITORY="ITSRizzoli/2021-23.SA.UFS07"
+
 
 if [ -z "${GITHUB_REF_NAME}" ];
 then
     echo "env GITHUB_REF_NAME is empty"
     exit 1
 fi
+echo "GITHUB_REF_NAME is $GITHUB_REF_NAME"
+
+
+if [ -z "${SOURCE_RUN_WORKFLOW_ID}" ];
+then
+    echo "env SOURCE_RUN_WORKFLOW_ID is empty"
+    exit 1
+fi
+echo "SOURCE_RUN_WORKFLOW_ID: $SOURCE_RUN_WORKFLOW_ID"
+
+gh run download $SOURCE_RUN_WORKFLOW_ID
+SOURCE_RUN_WORKFLOW_GITHUB_REF_NAME=`grep -oP "GITHUB_REF_NAME=\K.*" trigger_envs/trigger_envs.txt`
+echo "SOURCE_RUN_WORKFLOW_GITHUB_REF_NAME is $SOURCE_RUN_WORKFLOW_GITHUB_REF_NAME"
+
+IFS=$'\/' read -r prId _ <<< $SOURCE_RUN_WORKFLOW_GITHUB_REF_NAME
+echo "prId $prId"
 
 if [ -z "${GITHUB_API_URL}" ];
 then
     echo "env GITHUB_API_URL is empty"
     exit 1
 fi
+echo "GITHUB_API_URL is $GITHUB_API_URL"
 
 if [ -z "${GITHUB_REPOSITORY}" ];
 then
     echo "env GITHUB_REPOSITORY is empty"
     exit 1
 fi
+echo "GITHUB_REPOSITORY is $GITHUB_REPOSITORY"
 
-# retrieve pr id
-IFS=$'\/' read -r prId _ <<< $GITHUB_REF_NAME
-echo "prId $prId"
 
 ghApiGetReviews="$GITHUB_API_URL/repos/$GITHUB_REPOSITORY/pulls/$prId/reviews"
 echo "ghApiGetReviews: $ghApiGetReviews"
 gh api --method GET $ghApiGetReviews | jq '.' > ghApiGetReviewsResponse.ignore.json
 
-changesRequestedUrls=`cat ghApiGetReviewsResponse.ignore.json | jq -r '. | map(select(.state | contains ("CHANGES_REQUESTED"))) | .[] .html_url'`
-jq -ncR '[inputs]' <<< $changesRequestedUrls | jq '[ .[] | select(length > 0) ]' > changesRequestedUrls.ignore.json
+lastReviewState=`cat ghApiGetReviewsResponse.ignore.json | jq -r '. | last | .state'`
+echo "lastReviewState: $lastReviewState"
 
-cat changesRequestedUrls.ignore.json
-
-changesRequestedCount=`cat changesRequestedUrls.ignore.json | jq '. | length'`
-if [ "$changesRequestedCount" -ne "0" ];
+if [ "$lastReviewState" != "APPROVED" ];
 then
     echo "There are CHANGES REQUESTED ON THIS PR"
 
