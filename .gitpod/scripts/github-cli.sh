0daeaaa18218d2814d9ba4899bed5d86e5ff9523
aaglietti-itsrizzoli <a.aglietti@itsrizzoli.it>
2022-05-09 10:53:28 +0000

    fix(gitpod): reusable setup scripts

---
+++ .gitpod/scripts/github-cli.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Install GitHub CLI (gh)"
+
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
+sudo apt update && sudo apt install -y gh
+
+git config --global core.editor "code --wait"
---
+++ .gitpod/scripts/vscode-as-git-editor.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "VSCode will be used as commit and rebase interactive editor"
+
+git config --global sequence.editor "code --wait"
+
