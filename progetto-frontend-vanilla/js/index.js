73de8d2fccfc449ac54b18527c33d2622f651e61
Alessandro Aglietti <alessandro.aglietti@brumbrum.it>
2022-05-18 00:46:01 +0200

    feat(components-pipeline-4): react hello world

--- progetto-frontend-vanilla/js/index.js
+++ progetto-frontend-vanilla/js/index.js
@@ -1,9 +1,18 @@
 import "../css/index.css"
 import "../img/sun.png"
 import "../CHANGELOG.md"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
 function onClickAlert() {
     alert(`${new Date()}`);
 }
 
 window.onClickAlert = onClickAlert;
+
+const reactRoot = ReactDOM.createRoot(
+    document.getElementById("react-root")
+);
+
+reactRoot.render(React.createElement(App));
