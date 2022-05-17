dd068b8f20d4a42f3c8a4678ad575071621adcec
Alessandro Aglietti <alessandro.aglietti@brumbrum.it>
2022-05-18 00:46:21 +0200

    feat(components-pipeline-4): use our npm package

--- progetto-frontend-vanilla/js/App.jsx
+++ progetto-frontend-vanilla/js/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import {Button} from "test-1-react/atoms/button/Button"
 
 const App = () => ( 
     <div>
         <h1>Hello React</h1>
+        <Button label={"gasssss"} />
     </div>
 );
 
