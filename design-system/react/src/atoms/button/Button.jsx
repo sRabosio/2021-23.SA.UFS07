27d3c7e85d00fa23ff16bfb854387cc9fc284487
Alessandro Aglietti <alessandro.aglietti@brumbrum.it>
2022-05-17 16:47:23 +0200

    chore(components-pipeline-1): clean up button styling

--- design-system/react/src/atoms/button/Button.jsx
+++ design-system/react/src/atoms/button/Button.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './button.css';
 
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+export const Button = ({ label, onClick }) => {
   return (
     <button
+      onClick={onClick}
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={backgroundColor && { backgroundColor }}
-      {...props}
     >
       {label}
     </button>
@@ -20,18 +16,6 @@ export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
 };
 
 Button.propTypes = {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary: PropTypes.bool,
-  /**
-   * What background color to use
-   */
-  backgroundColor: PropTypes.string,
-  /**
-   * How large should the button be?
-   */
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
   /**
    * Button contents
    */
@@ -43,8 +27,5 @@ Button.propTypes = {
 };
 
 Button.defaultProps = {
-  backgroundColor: null,
-  primary: false,
-  size: 'medium',
   onClick: undefined,
 };
